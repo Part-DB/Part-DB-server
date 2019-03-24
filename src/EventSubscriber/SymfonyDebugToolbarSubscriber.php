ca8ef1d5bd8077a0927caff6a81d9c60fe817ea6
Jan Böhmer <Jan.h.boehmer@gmx.de>
2019-03-24 17:21:59 +0100

    Added an event subscriber, which injects a header helpful for debugging single page apps.

---
+++ src/EventSubscriber/SymfonyDebugToolbarSubscriber.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ *
+ * part-db version 0.1
+ * Copyright (C) 2005 Christoph Lechner
+ * http://www.cl-projects.de/
+ *
+ * part-db version 0.2+
+ * Copyright (C) 2009 K. Jacobs and others (see authors.php)
+ * http://code.google.com/p/part-db/
+ *
+ * Part-DB Version 0.4+
+ * Copyright (C) 2016 - 2019 Jan Böhmer
+ * https://github.com/jbtronics
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ */
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\EventDispatcher\Tests\Debug\EventSubscriber;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
+class SymfonyDebugToolbarSubscriber implements EventSubscriberInterface
+{
+
+    protected $kernel;
+
+    public function __construct(ContainerInterface $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
+    /**
+     * Returns an array of event names this subscriber wants to listen to.
+     *
+     * The array keys are event names and the value can be:
+     *
+     *  * The method name to call (priority defaults to 0)
+     *  * An array composed of the method name to call and the priority
+     *  * An array of arrays composed of the method names to call and respective
+     *    priorities, or 0 if unset
+     *
+     * For instance:
+     *
+     *  * ['eventName' => 'methodName']
+     *  * ['eventName' => ['methodName', $priority]]
+     *  * ['eventName' => [['methodName1', $priority], ['methodName2']]]
+     *
+     * @return array The event names to listen to
+     */
+    public static function getSubscribedEvents()
+    {
+        return ['kernel.response' => 'onKernelResponse'];
+    }
+
+
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        if (!$this->kernel->getParameter('kernel.debug')) {
+            return;
+        }
+
+        $response = $event->getResponse();
+        $response->headers->set('Symfony-Debug-Toolbar-Replace', 1);
+    }
+
+}
