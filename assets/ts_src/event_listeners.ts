528be3c00018e9a02729265a0310a8895254bfa0
Jan Böhmer <Jan.h.boehmer@gmx.de>
2019-03-26 16:40:05 +0100

    Added back to top button.

--- assets/ts_src/event_listeners.ts
+++ assets/ts_src/event_listeners.ts
@@ -128,6 +128,28 @@ $(document).on("ajaxUI:start ajaxUI:reload", function () {
 });
 
 
+/**
+ * Register the button, to jump to the top of the page.
+ */
+$(document).on("ajaxUI:start", function registerJumpToTop() {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    }).tooltip();
+});
+
+
 //Need for proper body padding, with every navbar height
 $(window).resize(function () {
     let height : number = $('#navbar').height() + 10;
