5d98e87e20c18b1698423200ad2de55c5b142753
Jan Böhmer <jan.h.boehmer@gmx.de>
2020-01-07 19:06:08 +0100

    Updated recipes for symfony/framework-bundle.

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@
  */
 
 use App\Kernel;
-use Symfony\Component\Debug\Debug;
+use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 require dirname(__DIR__).'/config/bootstrap.php';
