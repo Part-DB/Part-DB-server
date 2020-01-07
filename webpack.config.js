4e4b10ffbd85ee22d47a2be05f19983076edc2b6
Jan Böhmer <jan.h.boehmer@gmx.de>
2020-01-07 19:16:39 +0100

    Updated recipes of webpack-encore.

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,12 @@
 var Encore = require('@symfony/webpack-encore');
 const CopyPlugin = require('copy-webpack-plugin');
 
+// Manually configure the runtime environment if not already configured yet by the "encore" command.
+// It's useful when you use tools that rely on webpack.config.js file.
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
+
 Encore
     // directory where compiled assets will be stored
     .setOutputPath('public/build/')
@@ -66,6 +72,11 @@ Encore
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
 
+    // enables @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
     // enables Sass/SCSS support
     //.enableSassLoader()
 
