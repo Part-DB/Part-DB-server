e2ee0df58a0acef5ff339ed0e2255042e62540be
Jan Böhmer <jan.h.boehmer@gmx.de>
2020-06-03 21:29:40 +0200

    Added simple SMD reel calculator.

---
+++ src/Controller/ToolsController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/tools")
+ * @package App\Controller
+ */
+class ToolsController extends AbstractController
+{
+
+    /**
+     * @Route("/reel_calc", name="tools_reel_calculator")
+     * @return Response
+     */
+    public function reelCalculator() : Response
+    {
+        return $this->render("Tools/ReelCalculator/main.html.twig");
+    }
+}
