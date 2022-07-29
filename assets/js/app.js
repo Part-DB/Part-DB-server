8ccf5652abcec49ba2e6cc66bd480f87b75f73f5
Jan Böhmer <mail@jan-boehmer.de>
2022-07-30 00:33:22 +0200

    Implement darkmode using stimulus.

--- assets/js/app.js
+++ assets/js/app.js
@@ -50,6 +50,7 @@ import "./tristate_checkboxes";
 //Define jquery globally
 window.$ = window.jQuery = require("jquery")
 
+
 /**
 
  require('bootstrap-select');
@@ -94,25 +95,6 @@ require('../ts_src/ajax_ui');
 //Require all events;
 require('../ts_src/event_listeners');
 
-
-//Register darkmode (we must do it here, TS does not support ES6 constructor...
-try {
-    //The browser needs to support mix blend mode
-    if(typeof window.getComputedStyle(document.body).mixBlendMode !== 'undefined') {
-        const darkmode = new Darkmode();
-
-        $(document).on("ajaxUI:start ajaxUI:reload", function () {
-            //Show darkmode toggle to user
-            $('#toggleDarkmodeContainer, #toggleDarkmodeSeparator').removeAttr('hidden');
-            $('#toggleDarkmode').prop('checked', darkmode.isActivated());
-            $('#toggleDarkmode').change(function () {
-                darkmode.toggle();
-            });
-        });
-    }
-} catch (e) {
-    //Ignore all errors (for compatibiltiy with old browsers)
-}
 
 //Start AjaxUI AFTER all event has been registered
 //$(document).ready(ajaxUI.start());
