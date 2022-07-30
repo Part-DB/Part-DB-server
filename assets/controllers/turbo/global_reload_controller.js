9139d496284d63a8312a187067f8cb33f651bfe5
Jan Böhmer <mail@jan-boehmer.de>
2022-07-30 13:25:29 +0200

    Fixed global reload mechanism.

--- assets/controllers/turbo/global_reload_controller.js
+++ assets/controllers/turbo/global_reload_controller.js
@@ -3,6 +3,6 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
     connect() {
         //If we encounter an element with global reload controller, then reload the whole page
-        window.location.href = window.location.href;
+        window.location.reload();
     }
 }
