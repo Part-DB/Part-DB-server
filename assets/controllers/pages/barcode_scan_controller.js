ca5823819d6b51237023d10bfc79e2fdcea2e11a
Jan Böhmer <mail@jan-boehmer.de>
2022-08-13 22:41:54 +0200

    Use html5-qrcode instead of @zxing/library directly for label scanner.
    This improves the usability a bit

--- assets/controllers/pages/barcode_scan_controller.js
+++ assets/controllers/pages/barcode_scan_controller.js
@@ -1,86 +1,51 @@
 import {Controller} from "@hotwired/stimulus";
-import * as ZXing from "@zxing/library";
+//import * as ZXing from "@zxing/library";
+
+import {Html5QrcodeScanner, Html5Qrcode} from "html5-qrcode";
 
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
 
-    static targets = [ "source" ]
+    //codeReader = null;
 
-    codeReader = null;
+    _scanner = null;
+
 
     connect() {
         console.log('Init Scanner');
-        this.codeReader = new ZXing.BrowserMultiFormatReader();
-        this.initScanner();
+
+        //This function ensures, that the qrbox is 70% of the total viewport
+        let qrboxFunction = function(viewfinderWidth, viewfinderHeight) {
+            let minEdgePercentage = 0.7; // 70%
+            let minEdgeSize = Math.min(viewfinderWidth, viewfinderHeight);
+            let qrboxSize = Math.floor(minEdgeSize * minEdgePercentage);
+            return {
+                width: qrboxSize,
+                height: qrboxSize
+            };
+        }
+
+        //Try to get the number of cameras. If the number is 0, then the promise will fail, and we show the warning dialog
+        Html5Qrcode.getCameras().catch((devices) => {
+                document.getElementById('scanner-warning').classList.remove('d-none');
+        });
+
+        this._scanner = new Html5QrcodeScanner(this.element.id, {
+            fps: 2,
+            qrbox: qrboxFunction,
+            experimentalFeatures: {
+                //This option improves reading quality on android chrome
+                useBarCodeDetectorIfSupported: true
+            }
+        }, false);
+
+        this._scanner.render(this.onScanSuccess.bind(this));
     }
 
-    codeScannedHandler(result, err) {
-        if (result) {
-            //@ts-ignore
-            document.getElementById('scan_dialog_input').value = result.text;
-            //Submit form
-            //@ts-ignore
-            document.getElementById('scan_dialog_form').submit();
-        }
-        if (err && !(err instanceof ZXing.NotFoundException)) {
-            console.error(err);
-            //document.getElementById('result').textContent = err
-        }
-    }
-
-    initScanner() {
-        let selectedDeviceId;
-
-        this.codeReader.listVideoInputDevices()
-            .then((videoInputDevices) => {
-                if (videoInputDevices.length >= 1) {
-                    const sourceSelect = document.getElementById('sourceSelect');
-
-
-                    videoInputDevices.forEach((element) => {
-                        const sourceOption = document.createElement('option');
-                        sourceOption.text = element.label;
-                        sourceOption.value = element.deviceId;
-                        sourceSelect.appendChild(sourceOption);
-                    });
-
-                    //Try to retrieve last selected webcam...
-                    let last_cam_id = localStorage.getItem('scanner_last_cam_id');
-                    if (!!last_cam_id) {
-                        //selectedDeviceId = localStorage.getItem('scanner_last_cam_id');
-                        sourceSelect.value = last_cam_id;
-                    } else {
-                        selectedDeviceId = videoInputDevices[0].deviceId;
-                    }
-
-                    sourceSelect.onchange = () => {
-                        //@ts-ignore
-                        selectedDeviceId = sourceSelect.value;
-                        localStorage.setItem('scanner_last_cam_id', selectedDeviceId);
-                        changeHandler();
-                    };
-
-                    document.getElementById('sourceSelectPanel').classList.remove('d-none');
-                    document.getElementById('video').classList.remove('d-none');
-                    document.getElementById('scanner-warning').classList.add('d-none');
-                }
-
-
-                let changeHandler = () => {
-                    this.codeReader.reset();
-                    this.codeReader.decodeFromVideoDevice(selectedDeviceId, 'video', (result, err) => this.codeScannedHandler(result, err));
-                    console.log(`Started continous decode from camera with id ${selectedDeviceId}`)
-                };
-
-                //Register Change Src Button
-                //document.getElementById('changeSrcBtn').addEventListener('click', changeHandler);
-
-                //Try to start logging automatically.
-                changeHandler();
-
-            })
-            .catch((err) => {
-                console.error(err)
-            })
+    onScanSuccess(decodedText, decodedResult) {
+        //Put our decoded Text into the input box
+        document.getElementById('scan_dialog_input').value = decodedText;
+        //Submit form
+        document.getElementById('scan_dialog_form').submit();
     }
 }
