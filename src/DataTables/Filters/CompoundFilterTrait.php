ffa804404cce8aa0a660c346d417b42cc308e5c8
Jan Böhmer <mail@jan-boehmer.de>
2022-09-11 19:18:20 +0200

    Set compund filter accessible for reflection to fix errors on PHP < 8.1

--- src/DataTables/Filters/CompoundFilterTrait.php
+++ src/DataTables/Filters/CompoundFilterTrait.php
@@ -19,6 +19,9 @@ trait CompoundFilterTrait
         $reflection = new \ReflectionClass($this);
 
         foreach ($reflection->getProperties() as $property) {
+            //Set property to accessible (otherwise we run into problems on PHP < 8.1)
+            $property->setAccessible(true);
+
             $value = $property->getValue($this);
             //We only want filters (objects implementing FilterInterface)
             if($value instanceof FilterInterface) {
