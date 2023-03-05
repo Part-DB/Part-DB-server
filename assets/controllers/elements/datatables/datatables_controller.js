02134dc959a27fc355a257b57e69c55695b3cef9
Jan Böhmer <mail@jan-boehmer.de>
2023-03-05 01:11:57 +0100

    Do not persist the selected datatable page number, as we always want to start at the first page after a page reload.

--- assets/controllers/elements/datatables/datatables_controller.js
+++ assets/controllers/elements/datatables/datatables_controller.js
@@ -66,7 +66,12 @@ export default class extends Controller {
     }
 
     stateLoadCallback(settings) {
-        return JSON.parse( localStorage.getItem(this.getStateSaveKey()) );
+        const data = JSON.parse( localStorage.getItem(this.getStateSaveKey()) );
+
+        //Do not save the start value (current page), as we want to always start at the first page on a page reload
+        data.start = 0;
+
+        return data;
     }
 
     connect() {
