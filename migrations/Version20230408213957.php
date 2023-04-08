cc033d5be7973a2d940dccedead8105b9389ae8c
Jan Böhmer <mail@jan-boehmer.de>
2023-04-09 00:08:08 +0200

    Properly escape users and groups table name for newer MySQL version

--- migrations/Version20230408213957.php
+++ migrations/Version20230408213957.php
@@ -21,14 +21,14 @@ final class Version20230408213957 extends AbstractMultiPlatformMigration
     public function mySQLUp(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE groups CHANGE permissions_data permissions_data LONGTEXT DEFAULT \'[]\' NOT NULL COMMENT \'(DC2Type:json)\'');
-        $this->addSql('ALTER TABLE log CHANGE level level TINYINT(4) NOT NULL');
+        $this->addSql('ALTER TABLE `groups` CHANGE permissions_data permissions_data LONGTEXT DEFAULT \'[]\' NOT NULL COMMENT \'(DC2Type:json)\'');
+        $this->addSql('ALTER TABLE `log` CHANGE level level TINYINT(4) NOT NULL');
         if ($this->doesFKExists('projects', 'FK_11074E9A727ACA70')){
             $this->addSql('ALTER TABLE projects DROP FOREIGN KEY FK_11074E9A727ACA70');
         }
         $this->addSql('ALTER TABLE projects CHANGE description description LONGTEXT DEFAULT \'\' NOT NULL');
-        $this->addSql('ALTER TABLE users CHANGE permissions_data permissions_data LONGTEXT DEFAULT \'[]\' NOT NULL COMMENT \'(DC2Type:json)\', CHANGE saml_user saml_user TINYINT(1) NOT NULL, CHANGE about_me about_me LONGTEXT DEFAULT \'\' NOT NULL');
-        $this->addSql('ALTER TABLE log CHANGE level level TINYINT NOT NULL COMMENT \'(DC2Type:tinyint)\'');
+        $this->addSql('ALTER TABLE `users` CHANGE permissions_data permissions_data LONGTEXT DEFAULT \'[]\' NOT NULL COMMENT \'(DC2Type:json)\', CHANGE saml_user saml_user TINYINT(1) NOT NULL, CHANGE about_me about_me LONGTEXT DEFAULT \'\' NOT NULL');
+        $this->addSql('ALTER TABLE `log` CHANGE level level TINYINT NOT NULL COMMENT \'(DC2Type:tinyint)\'');
     }
 
     public function mySQLDown(Schema $schema): void
