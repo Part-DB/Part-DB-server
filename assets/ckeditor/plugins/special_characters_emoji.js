bc65a18f3c31e044e1796318a92d689866d0d862
Jan Böhmer <mail@jan-boehmer.de>
2023-04-23 21:19:53 +0200

    Added greek characters to special characters plugin in CKEDITOR
    Fixes #275

--- assets/ckeditor/plugins/special_characters_emoji.js
+++ assets/ckeditor/plugins/special_characters_emoji.js
@@ -30,7 +30,71 @@ export default class SpecialCharactersEmoji extends Plugin {
         const editor = this.editor;
         const specialCharsPlugin = editor.plugins.get('SpecialCharacters');
 
+        //Add greek characters to special characters
+        specialCharsPlugin.addItems('Greek', this.getGreek());
+
+        //Add Emojis to special characters
         specialCharsPlugin.addItems('Emoji', this.getEmojis());
+    }
+
+    getGreek() {
+        return [
+            { title: 'Alpha', character: 'Α' },
+            { title: 'Beta', character: 'Β' },
+            { title: 'Gamma', character: 'Γ' },
+            { title: 'Delta', character: 'Δ' },
+            { title: 'Epsilon', character: 'Ε' },
+            { title: 'Zeta', character: 'Ζ' },
+            { title: 'Eta', character: 'Η' },
+            { title: 'Theta', character: 'Θ' },
+            { title: 'Iota', character: 'Ι' },
+            { title: 'Kappa', character: 'Κ' },
+            { title: 'Lambda', character: 'Λ' },
+            { title: 'Mu', character: 'Μ' },
+            { title: 'Nu', character: 'Ν' },
+            { title: 'Xi', character: 'Ξ' },
+            { title: 'Omicron', character: 'Ο' },
+            { title: 'Pi', character: 'Π' },
+            { title: 'Rho', character: 'Ρ' },
+            { title: 'Sigma', character: 'Σ' },
+            { title: 'Tau', character: 'Τ' },
+            { title: 'Upsilon', character: 'Υ' },
+            { title: 'Phi', character: 'Φ' },
+            { title: 'Chi', character: 'Χ' },
+            { title: 'Psi', character: 'Ψ' },
+            { title: 'Omega', character: 'Ω' },
+            { title: 'alpha', character: 'α' },
+            { title: 'beta', character: 'β' },
+            { title: 'gamma', character: 'γ' },
+            { title: 'delta', character: 'δ' },
+            { title: 'epsilon', character: 'ε' },
+            { title: 'zeta', character: 'ζ' },
+            { title: 'eta', character: 'η' },
+            { title: 'theta', character: 'θ' },
+            { title: 'alternate theta', character: 'ϑ' },
+            { title: 'iota', character: 'ι' },
+            { title: 'kappa', character: 'κ' },
+            { title: 'lambda', character: 'λ' },
+            { title: 'mu', character: 'μ' },
+            { title: 'nu', character: 'ν' },
+            { title: 'xi', character: 'ξ' },
+            { title: 'omicron', character: 'ο' },
+            { title: 'pi', character: 'π' },
+            { title: 'rho', character: 'ρ' },
+            { title: 'sigma', character: 'σ' },
+            { title: 'tau', character: 'τ' },
+            { title: 'upsilon', character: 'υ' },
+            { title: 'phi', character: 'φ' },
+            { title: 'chi', character: 'χ' },
+            { title: 'psi', character: 'ψ' },
+            { title: 'omega', character: 'ω' },
+            { title: 'digamma', character: 'Ϝ' },
+            { title: 'stigma', character: 'Ϛ' },
+            { title: 'heta', character: 'Ͱ' },
+            { title: 'sampi', character: 'Ϡ' },
+            { title: 'koppa', character: 'Ϟ' },
+            { title: 'san', character: 'Ϻ' },
+        ];
     }
 
     getEmojis() {
