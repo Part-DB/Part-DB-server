edce70bc12b126a511a77163462a8725ccdd06fa
Jan Böhmer <mail@jan-boehmer.de>
2023-05-27 20:35:36 +0200

    Updated symfony to 6.2, updated scheb/2fa bundle and removed obsolete hslavich/oneloginsaml-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -27,5 +27,4 @@ return [
     Scheb\TwoFactorBundle\SchebTwoFactorBundle::class => ['all' => true],
     SpomkyLabs\CborBundle\SpomkyLabsCborBundle::class => ['all' => true],
     Webauthn\Bundle\WebauthnBundle::class => ['all' => true],
-    Hslavich\OneloginSamlBundle\HslavichOneloginSamlBundle::class => ['all' => true],
 ];
