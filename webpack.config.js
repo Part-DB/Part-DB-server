7658cfcdbd42e032f13beadffb9ae76c31a1b582
Jan Böhmer <mail@jan-boehmer.de>
2023-06-11 19:58:31 +0200

    Updated symfony/stimulus-bridge recipe

--- webpack.config.js
+++ webpack.config.js
@@ -71,6 +71,9 @@ Encore
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
 
+    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
+    .enableStimulusBridge('./assets/controllers.json')
+
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
