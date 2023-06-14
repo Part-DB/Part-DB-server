2f46fbfc7a4ad6d474ea8b4c864f3a63512a6511
Jan Böhmer <mail@jan-boehmer.de>
2023-06-14 23:14:49 +0200

    Added stricter phpstan checks

--- src/DataTables/Filters/Constraints/InstanceOfConstraint.php
+++ src/DataTables/Filters/Constraints/InstanceOfConstraint.php
@@ -94,6 +94,7 @@ class InstanceOfConstraint extends AbstractConstraint
 
         $expressions = [];
 
+        /** @phpstan-ignore-next-line */
         if ($this->operator === 'ANY' || $this->operator === 'NONE') {
             foreach($this->value as $value) {
                 //We can not use a parameter here, as this is the only way to pass the FCQN to the query (via binded params, we would need to use ClassMetaData). See: https://github.com/doctrine/orm/issues/4462
