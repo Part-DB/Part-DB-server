03712fcf967f17c5dd2566096a0a1d7686d4a7e2
Jan Böhmer <mail@jan-boehmer.de>
2023-11-27 23:17:20 +0100

    Show an error flash, if the info providers cannnot communicate with the servers instead of throwing an exception

--- src/Controller/InfoProviderController.php
+++ src/Controller/InfoProviderController.php
@@ -33,13 +33,17 @@ use App\Services\InfoProviderSystem\ProviderRegistry;
 use App\Services\LogSystem\EventCommentHelper;
 use App\Services\Parts\PartFormHelper;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpClient\Exception\ClientException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Translation\TranslatorInterface;
+
+use function Symfony\Component\Translation\t;
 
 #[Route('/tools/info_providers')]
 class InfoProviderController extends  AbstractController
@@ -64,7 +68,7 @@ class InfoProviderController extends  AbstractController
 
     #[Route('/search', name: 'info_providers_search')]
     #[Route('/update/{target}', name: 'info_providers_update_part_search')]
-    public function search(Request $request, #[MapEntity(id: 'target')] ?Part $update_target): Response
+    public function search(Request $request, #[MapEntity(id: 'target')] ?Part $update_target, LoggerInterface $exceptionLogger): Response
     {
         $this->denyAccessUnlessGranted('@info_providers.create_parts');
 
@@ -83,7 +87,14 @@ class InfoProviderController extends  AbstractController
             $keyword = $form->get('keyword')->getData();
             $providers = $form->get('providers')->getData();
 
-            $results = $this->infoRetriever->searchByKeyword(keyword: $keyword, providers: $providers);
+            try {
+                $results = $this->infoRetriever->searchByKeyword(keyword: $keyword, providers: $providers);
+            } catch (ClientException $e) {
+                $this->addFlash('error', t('info_providers.search.error.client_exception'));
+                $this->addFlash('error',$e->getMessage());
+                //Log the exception
+                $exceptionLogger->error('Error during info provider search: ' . $e->getMessage(), ['exception' => $e]);
+            }
         }
 
         return $this->render('info_providers/search/part_search.html.twig', [
