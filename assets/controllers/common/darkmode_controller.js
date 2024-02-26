4398b8698c6bea527169eac1bab788d7d53159d3
Jan Böhmer <mail@jan-boehmer.de>
2024-02-27 00:33:48 +0100

    Set proper darkmode attribute for algolia autocomplete

--- assets/controllers/common/darkmode_controller.js
+++ assets/controllers/common/darkmode_controller.js
@@ -91,6 +91,9 @@ export default class extends Controller {
     _enableDarkmode() {
         //Add data-bs-theme="dark" to the html tag
         document.documentElement.setAttribute('data-bs-theme', 'dark');
+
+        //Add data-theme="dark" to the body tag for algolia autocomplete
+        document.body.setAttribute('data-theme', 'dark');
     }
 
     /**
@@ -100,6 +103,9 @@ export default class extends Controller {
     _disableDarkmode() {
         //Set data-bs-theme to light
         document.documentElement.setAttribute('data-bs-theme', 'light');
+
+        //Add data-theme="dark" to the body tag for algolia autocomplete
+        document.body.setAttribute('data-theme', 'light');
     }
 
 
