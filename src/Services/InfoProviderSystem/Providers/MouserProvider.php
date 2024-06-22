f6e955b487a13cda3192a283290a5e7827037568
Jan Böhmer <mail@jan-boehmer.de>
2024-06-22 23:37:50 +0200

    Hide mouser parts with an invalid product ID to avoid the issue described in #616

--- src/Services/InfoProviderSystem/Providers/MouserProvider.php
+++ src/Services/InfoProviderSystem/Providers/MouserProvider.php
@@ -177,7 +177,7 @@ class MouserProvider implements InfoProviderInterface
         }
 
         if (count($tmp) > 1) {
-            throw new \RuntimeException('Multiple parts found with ID '.$id);
+            throw new \RuntimeException('Multiple parts found with ID '.$id . ' ('.count($tmp).' found). This is basically a bug in Mousers API response. See issue #616.');
         }
 
         return $tmp[0];
@@ -210,6 +210,12 @@ class MouserProvider implements InfoProviderInterface
 
         $result = [];
         foreach ($products as $product) {
+
+            //Check if we have a valid product number. We assume that a product number, must have at least 4 characters
+            //Otherwise filter it out
+            if (strlen($product['MouserPartNumber']) < 4) {
+                continue;
+            }
 
             //Check if we have a mass field available
             $mass = null;
