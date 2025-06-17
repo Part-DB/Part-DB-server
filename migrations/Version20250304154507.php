2066d20edfe7e50d6c645135e670ebbca0e270fa
Marcel Diegelmann <marcel.diegelmann@gmail.com>
2025-06-17 14:02:30 +0200

    SQL-Formatierung in Migration verbessern

--- migrations/Version20250304154507.php
+++ migrations/Version20250304154507.php
@@ -17,16 +17,28 @@ final class Version20250304154507 extends AbstractMultiPlatformMigration
 
     public function mySQLUp(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE parts ADD built_assembly_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE parts ADD CONSTRAINT FK_6940A7FECC660B3C FOREIGN KEY (built_assembly_id) REFERENCES assemblies (id)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_6940A7FECC660B3C ON parts (built_assembly_id)');
+        $this->addSql(<<<'SQL'
+            ALTER TABLE parts ADD built_assembly_id INT DEFAULT NULL
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE parts ADD CONSTRAINT FK_6940A7FECC660B3C FOREIGN KEY (built_assembly_id) REFERENCES assemblies (id)
+        SQL);
+        $this->addSql(<<<'SQL'
+            CREATE UNIQUE INDEX UNIQ_6940A7FECC660B3C ON parts (built_assembly_id)
+        SQL);
     }
 
     public function mySQLDown(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE `parts` DROP FOREIGN KEY FK_6940A7FECC660B3C');
-        $this->addSql('DROP INDEX UNIQ_6940A7FECC660B3C ON `parts`');
-        $this->addSql('ALTER TABLE `parts` DROP built_assembly_id');
+        $this->addSql(<<<'SQL'
+            ALTER TABLE parts DROP FOREIGN KEY FK_6940A7FECC660B3C
+        SQL);
+        $this->addSql(<<<'SQL'
+            DROP INDEX UNIQ_6940A7FECC660B3C ON parts
+        SQL);
+        $this->addSql(<<<'SQL'
+            ALTER TABLE `parts` DROP built_assembly_id
+        SQL);
     }
 
     public function sqLiteUp(Schema $schema): void
