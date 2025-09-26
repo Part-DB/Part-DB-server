bfd06bb5343e49fc957c6943260356ba252402ff
Marcel Diegelmann <marcel.diegelmann@gmail.com>
2025-09-26 14:07:35 +0200

    PartCustomStateControllerTest: Attribute für PHPUnit-Gruppen umgestellt

--- tests/Controller/AdminPages/PartCustomStateControllerTest.php
+++ tests/Controller/AdminPages/PartCustomStateControllerTest.php
@@ -23,12 +23,11 @@ declare(strict_types=1);
 namespace App\Tests\Controller\AdminPages;
 
 use App\Entity\Parts\PartCustomState;
+use PHPUnit\Framework\Attributes\Group;
 
-/**
- * @group slow
- * @group DB
- */
-class PartCustomStateControllerTest extends AbstractAdminControllerTest
+#[Group('slow')]
+#[Group('DB')]
+class PartCustomStateControllerTest extends AbstractAdminController
 {
     protected static string $base_path = '/en/part_custom_state';
     protected static string $entity_class = PartCustomState::class;
