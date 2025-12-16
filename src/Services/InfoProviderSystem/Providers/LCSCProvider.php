088ff9eb458d935aa0578fe9058b9221995e1abf
Marc Kreidler <kreidler@nedo.com>
2025-12-16 20:57:14 +0100

    https://github.com/Part-DB/Part-DB-server/pull/1151/files/edd5fb3319e9c8c4470476a70b6f398e3d545550#r2622249199 Revert "Change order of capabilities in LCSCProvider.php"
    This reverts commit dfd6f33e52299377f3537285b2ff0bc77fa6e1af.

--- src/Services/InfoProviderSystem/Providers/LCSCProvider.php
+++ src/Services/InfoProviderSystem/Providers/LCSCProvider.php
@@ -446,10 +446,10 @@ class LCSCProvider implements BatchInfoProviderInterface
     {
         return [
             ProviderCapabilities::BASIC,
-            ProviderCapabilities::FOOTPRINT,
             ProviderCapabilities::PICTURE,
             ProviderCapabilities::DATASHEET,
             ProviderCapabilities::PRICE,
+            ProviderCapabilities::FOOTPRINT,
         ];
     }
 }
