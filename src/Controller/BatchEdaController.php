e05a751b84a8c3cd0426134aaacdf3a4a7cb2108
Sebastian Almberg <83243306+Sebbeben@users.noreply.github.com>
2026-02-09 12:19:26 +0100

    Remove unused counter variable in BatchEdaController

--- src/Controller/BatchEdaController.php
+++ src/Controller/BatchEdaController.php
@@ -40,43 +40,33 @@ class BatchEdaController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $updated = 0;
-
             foreach ($parts as $part) {
                 $this->denyAccessUnlessGranted('edit', $part);
                 $edaInfo = $part->getEdaInfo();
 
                 if ($form->get('apply_reference_prefix')->getData()) {
                     $edaInfo->setReferencePrefix($form->get('reference_prefix')->getData() ?: null);
-                    $updated++;
                 }
                 if ($form->get('apply_value')->getData()) {
                     $edaInfo->setValue($form->get('value')->getData() ?: null);
-                    $updated++;
                 }
                 if ($form->get('apply_kicad_symbol')->getData()) {
                     $edaInfo->setKicadSymbol($form->get('kicad_symbol')->getData() ?: null);
-                    $updated++;
                 }
                 if ($form->get('apply_kicad_footprint')->getData()) {
                     $edaInfo->setKicadFootprint($form->get('kicad_footprint')->getData() ?: null);
-                    $updated++;
                 }
                 if ($form->get('apply_visibility')->getData()) {
                     $edaInfo->setVisibility($form->get('visibility')->getData());
-                    $updated++;
                 }
                 if ($form->get('apply_exclude_from_bom')->getData()) {
                     $edaInfo->setExcludeFromBom($form->get('exclude_from_bom')->getData());
-                    $updated++;
                 }
                 if ($form->get('apply_exclude_from_board')->getData()) {
                     $edaInfo->setExcludeFromBoard($form->get('exclude_from_board')->getData());
-                    $updated++;
                 }
                 if ($form->get('apply_exclude_from_sim')->getData()) {
                     $edaInfo->setExcludeFromSim($form->get('exclude_from_sim')->getData());
-                    $updated++;
                 }
             }
 
