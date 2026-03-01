232c973a1884278e8c26ede0ef209226dff49025
Jan Böhmer <mail@jan-boehmer.de>
2026-03-01 22:23:36 +0100

    Use better labels for column names and made it visible as default column selection

--- src/DataTables/PartsDataTable.php
+++ src/DataTables/PartsDataTable.php
@@ -229,14 +229,14 @@ final class PartsDataTable implements DataTableTypeInterface
             ->add('attachments', PartAttachmentsColumn::class, [
                 'label' => $this->translator->trans('part.table.attachments'),
             ])
-            ->add('eda_info_reference_prefix', TextColumn::class, [
-                'label' => $this->translator->trans('eda_info.reference_prefix'),
-                'render' => fn($value, Part $context) => htmlspecialchars($context->getEdaInfo()->getReferencePrefix() ?? ''),
+            ->add('eda_reference', TextColumn::class, [
+                'label' => $this->translator->trans('part.table.eda_reference'),
+                'render' => static fn($value, Part $context) => htmlspecialchars($context->getEdaInfo()->getReferencePrefix() ?? ''),
                 'orderField' => 'NATSORT(part.eda_info.reference_prefix)'
             ])
-            ->add('eda_info_value', TextColumn::class, [
-                'label' => $this->translator->trans('eda_info.value'),
-                'render' => fn($value, Part $context) => htmlspecialchars($context->getEdaInfo()->getValue() ?? ''),
+            ->add('eda_value', TextColumn::class, [
+                'label' => $this->translator->trans('part.table.eda_value'),
+                'render' => static fn($value, Part $context) => htmlspecialchars($context->getEdaInfo()->getValue() ?? ''),
                 'orderField' => 'NATSORT(part.eda_info.value)'
             ]);
 
