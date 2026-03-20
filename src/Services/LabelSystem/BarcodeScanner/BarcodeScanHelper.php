e045cf0eb5b11d2ab1019b47f780357a82995cde
alufers <alufers@wp.pl>
2026-03-20 23:09:06 +0100

    Implement parsing of TME QR codes
    They are present on parts purchased on tme.eu. It's based on the LCSC
    parser. Some older codes I found are in upper-case so I handle those
    too.

--- src/Services/LabelSystem/BarcodeScanner/BarcodeScanHelper.php
+++ src/Services/LabelSystem/BarcodeScanner/BarcodeScanHelper.php
@@ -105,6 +105,10 @@ final class BarcodeScanHelper
             return new AmazonBarcodeScanResult($input);
         }
 
+        if ($type === BarcodeSourceType::TME) {
+            return TMEBarcodeScanResult::parse($input);
+        }
+
         //Null means auto and we try the different formats
         $result = $this->parseInternalBarcode($input);
 
@@ -144,6 +148,11 @@ final class BarcodeScanHelper
             return new AmazonBarcodeScanResult($input);
         }
 
+        // Try TME barcode
+        if (TMEBarcodeScanResult::isTMEBarcode($input)) {
+            return TMEBarcodeScanResult::parse($input);
+        }
+
         throw new InvalidArgumentException('Unknown barcode');
     }
 
@@ -160,6 +169,11 @@ final class BarcodeScanHelper
     private function parseLCSCBarcode(string $input): LCSCBarcodeScanResult
     {
         return LCSCBarcodeScanResult::parse($input);
+    }
+
+    private function parseTMEBarcode(string $input): TMEBarcodeScanResult
+    {
+        return TMEBarcodeScanResult::parse($input);
     }
 
     private function parseUserDefinedBarcode(string $input): ?LocalBarcodeScanResult
