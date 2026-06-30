2736b4710358c3169393647910c1023fa11e9101
buchmann <buchmann@ticmed.de>
2026-06-30 14:53:33 +0200

    Consider mutual exclusivity of search options
    If regex search is enabled, the other two options are disabled (only visually). This should give the user a fair idea of what's happening while keeping things as simple as possible.

---
+++ assets/controllers/elements/search_config_controller.js
@@ -0,0 +1,47 @@
+/*
+ * This file is part of Part-DB (https://github.com/Part-DB/Part-DB-symfony).
+ *
+ *  Copyright (C) 2019 - 2026 Jan Böhmer (https://github.com/jbtronics)
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+import {Controller} from "@hotwired/stimulus";
+
+export default class extends Controller
+{
+    connect() {
+        this.element.addEventListener('change', () => {
+            this.updateOthers()
+        });
+    }
+
+    updateOthers() {
+        let value = this.element.checked
+        let ext = document.getElementById('extensive')
+        let wild = document.getElementById('wildcard')
+        if (value === null) {
+            return;
+        }
+
+        if (value) {
+            ext.disabled = 'disabled'
+            wild.disabled = 'disabled'
+        }
+        else {
+            ext.removeAttribute('disabled')
+            wild.removeAttribute('disabled')
+        }
+    }
+}
