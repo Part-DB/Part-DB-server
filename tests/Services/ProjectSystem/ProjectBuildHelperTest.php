7458e698bf1aabc9e4837a80e91fe86033835d3c
buchmann <buchmann@ticmed.de>
2026-07-27 12:31:18 +0200

    fix test

--- tests/Services/ProjectSystem/ProjectBuildHelperTest.php
+++ tests/Services/ProjectSystem/ProjectBuildHelperTest.php
@@ -217,15 +217,15 @@ final class ProjectBuildHelperTest extends WebTestCase
         $project->addBomEntry($entry);
         
         $subproject = new Project();
-        $subproject->setParent($project);
         $subentry = new ProjectBOMEntry();
         $subentry->setPart($this->makePartWithPrice(3.5));
         $subentry->setQuantity(2);
         
+        $part = new Part();
         $entry2 = new ProjectBOMEntry();
-        $entry2->setPart(new Part());
+        $part->setBuiltProject($subproject);
+        $entry2->setPart($part);
         $entry2->setQuantity(2);
-        $subproject->setBuildPart($entry2->getPart());
         $project->addBomEntry($entry2);
 
         // 4 × 1.50 + 2 x (2 x 3.5) = 20.00 for 1 build
